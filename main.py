86efcb93a6011b9fbbe44d49d2e742b156aa739f
PraxTube <lukinator28@gmail.com>
2024-01-09 19:36:04 +0100

    feat: Improve plot and add moving average

--- main.py
+++ main.py
@@ -1,42 +1,68 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import calendar
 
-df = pd.read_csv(
-    "~/.config/dogg/data/food.csv",
-    header=None,
-    names=["Timestamp", "Column1", "Column2", "Meal", "Boolean", "Kcal"],
-)
 
-# Convert 'Timestamp' column to datetime with considering timezone
-df["Timestamp"] = pd.to_datetime(df["Timestamp"], utc=True)
+def daily_cals():
+    df = pd.read_csv(
+        "~/.config/dogg/data/food.csv",
+        header=None,
+        names=["Timestamp", "Column1", "Column2", "Meal", "Boolean", "Kcal"],
+    )
 
-# Extract date from timestamp and create a new 'Date' column
-df["Date"] = df["Timestamp"].dt.tz_convert(None).dt.date
+    # Convert 'Timestamp' column to datetime with considering timezone
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"], utc=True)
+    # Extract date from timestamp and create a new 'Date' column
+    df["Date"] = df["Timestamp"].dt.tz_convert(None).dt.date
+    # Group by 'Date' and calculate total calories for each day
+    raw_daily_calories = df.groupby("Date")["Kcal"].sum()
 
-# Group by 'Date' and calculate total calories for each day
-raw_daily_calories = df.groupby("Date")["Kcal"].sum()
+    # Find the index where the calories are non-zero
+    start_index = raw_daily_calories[raw_daily_calories != 0].index[0]
+    # Filter the dataset to start from the first non-zero calories
+    daily_calories = raw_daily_calories[start_index:]
+    return daily_calories
 
-# Find the index where the calories are non-zero
-start_index = raw_daily_calories[raw_daily_calories != 0].index[0]
 
-# Filter the dataset to start from the first non-zero calories
-daily_calories = raw_daily_calories[start_index:]
+def average_cals():
+    cumulative_sum = daily_cals().cumsum()
+    moving_average = cumulative_sum / range(1, len(cumulative_sum) + 1)
+    return moving_average
 
-unique_months = set(date.strftime("%Y-%m-01") for date in daily_calories.index)
-first_day_of_month = pd.to_datetime(list(unique_months))
 
-# Plotting
-plt.figure(figsize=(14, 6))
-plt.bar(daily_calories.index, daily_calories, color="skyblue", width=0.8)
+def plot():
+    daily_calories = daily_cals()
+    moving_average = average_cals()
+    unique_months = set(date.strftime("%Y-%m-01") for date in daily_calories.index)
+    first_day_of_month = pd.to_datetime(list(unique_months))
 
-# Get month names to display on x-axis ticks
-month_names = [calendar.month_name[date.month] for date in first_day_of_month]
-plt.xticks(first_day_of_month, month_names, rotation=45)
+    # Plotting
+    plt.figure(figsize=(14, 6))
+    plt.bar(daily_calories.index, daily_calories, color="skyblue", width=0.8)
 
-plt.xlabel("Date")
-plt.ylabel("Total Calories")
-plt.title("Total Calories Consumed (Daily) with Single Tick for Each Month")
-plt.tight_layout()
-plt.grid()
-plt.show()
+    # Plot the moving average as a line
+    plt.plot(moving_average.index, moving_average, color='red', label='Moving Average')
+
+    # Get month names to display on x-axis ticks
+    month_names = [calendar.month_name[date.month] for date in first_day_of_month]
+    plt.xticks(first_day_of_month, month_names)
+
+    y_min = np.floor(min(daily_calories) / 500) * 500
+    y_max = np.ceil(max(daily_calories) / 500) * 500
+    plt.yticks(np.arange(y_min, y_max, step=500))
+    plt.ylim(y_min - 100, y_max + 100)
+
+    plt.ylabel("Total Calories in kcal")
+    plt.title("Total Calories Consumed (Daily) with Single Tick for Each Month")
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
+
+
+def main():
+    plot()
+
+
+if __name__ == "__main__":
+    main()
